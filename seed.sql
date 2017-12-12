d9e77f2af04cb23abc1dabb75e6de76a790bcf5c
boronflex <kyle_palmer@icloud.com>
2017-12-12 16:30:26 -0600

    added files and started sql seed file

---
+++ seed.sql
@@ -0,0 +1,36 @@
+--  1. Create a MySQL Database called `bamazon`.
+
+DROP DATABASE IF EXISTS bamazon;
+CREATE DATABASE bamazon;
+
+USE bamazon;
+
+--  2. Then create a Table inside of that database called `products`.
+
+CREATE TABLE products(
+
+--  3. The products table should have each of the following columns:
+
+--     * item_id (unique id for each product)
+
+  item_id INT NOT NULL AUTO_INCREMENT,
+
+--     * product_name (Name of product)
+
+  product_name VARCHAR(100) NOT NULL,
+
+--     * department_name
+
+  department_name VARCHAR(30) NOT NULL,
+
+--     * price (cost to customer)
+
+  price DECIMAL(10,2) NOT NULL,
+
+--     * stock_quantity (how much of the product is available in stores)
+
+  stock_quantity INT NULL,
+
+)
+
+--  4. Populate this database with around 10 different products. (i.e. Insert "mock" data rows into this database and table).
