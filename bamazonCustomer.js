cc0797a6fd8566fef7ecbb0d37af8838edd5db07
boronflex <kyle_palmer@icloud.com>
2017-12-12 17:47:56 -0600

    added some pseudocode for coming back to

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,6 @@
 // 5. Then create a Node application called `bamazonCustomer.js`.
 
+
 var inquirer = require("inquirer");
 var mysql = require("mysql");
 
@@ -20,29 +21,35 @@ connection.connect(function(err) {
   //customerOrder(); run app if connection successful
 });
 
-
 // 6. The app should then prompt users with two messages.
 
 function customerOrder(){
 
-//Running this application will first display all of the items available for sale. Include the ids, names, 
+//Running this application will first display all of the items available for sale. Include the ids, names,
 //and prices of products for sale.
+  //SELECT * FROM products;
   
-    inquirer.prompt([
+    inquirer.prompt([// consider throwing an autocomplete in here https://github.com/mokkabonna/inquirer-autocomplete-prompt
+
+      //also there is a validate feature i could use here in the docs, returns promise
+      //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise
 
 //    * The first should ask them the ID of the product they would like to buy.
 
       
         {
-          type: "confirm",
-          name: "userInput",
-          message: "Do you want play again?",
+          type: "list",
+          name: "productID",
+          message: "Which item are you interested in purchasing?",
+          choices: [1,2,3,4]
         },
 
 //    * The second message should ask how many units of the product they would like to buy.
 
         {
-
+          type: "input",
+          name: "productAMT",
+          message: "How many do you want to purchase?",
         }
     
       
@@ -69,6 +76,11 @@ function customerOrder(){
 
 //    * If not, the app should log a phrase like `Insufficient quantity!`, and then prevent the order from going through.
 
+    //need order constructor to pass orders to - validates this crap before it runs sql
+    //SELECT query validate amout is greater than requested
+
 // 8. However, if your store _does_ have enough of the product, you should fulfill the customer's order.
 //    * This means updating the SQL database to reflect the remaining quantity.
+    //UPDATE query here
 //    * Once the update goes through, show the customer the total cost of their purchase.
+    //function probably
